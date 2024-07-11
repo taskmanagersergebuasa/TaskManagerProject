7f7b29fb4b784507802e68b954dad9302be9cb8c
laetitia <dauchyla@gmail.com>
2024-07-11 10:01:23 +0200

    ajout squelette méthodes de nettoyage

--- formationscraper/formationscraper/pipelines.py
+++ formationscraper/formationscraper/pipelines.py
@@ -6,18 +6,14 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import re
 import csv
-
-
-class FormationscraperPipeline:
-    def process_item(self, item, spider):
-        return item
 
 
 class CsvPipeline:
     def open_spider(self, spider):
         # Ouverture du fichier en mode écriture
-        self.file = open('formation6.csv', 'w', newline='', encoding='utf-8')
+        self.file = open('formation_for_clean.csv', 'w', newline='', encoding='utf-8')
         
         # Définition des en-têtes
         fieldnames = ['filiere', 'titre_formation', 'certif_rncp', 'certif_rs', 'certif_fp_rnpc', 'titre_certif_fp_rncp', 'etat_fp_rncp', 'niveau_fp_rncp', 'nsf_fp_rncp', 'formacode_fp_rncp', 'certif_fp_rs', 'titre_certif_fp_rs', 'etat_fp_rs', 'nsf_fp_rs', 'formacode_fp_rs' ]
@@ -36,3 +32,23 @@ class CsvPipeline:
         # Écriture des données
         self.writer.writerow(item)
         return item
+
+
+class FormationscraperPipeline:
+    def process_item(self, item, spider):
+        self.clean_niveau_fp_rncp(item)
+        self.clean_nsf(item)
+        self.clean_formacode(item)
+        return item
+    
+    def clean_niveau_fp_rncp(self, item):
+        pass
+
+    def clean_nsf(self, item):
+        pass
+    
+    def clean_formacode(self, item):
+        pass
+
+
+    
