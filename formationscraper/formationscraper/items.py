9928c8249ef8b669b2394bb5fb9099555a55c785
EliasMez <elias.mezine@laposte.net>
2024-07-11 11:09:06 +0200

    add session scraping and item

--- formationscraper/formationscraper/items.py
+++ formationscraper/formationscraper/items.py
@@ -7,11 +7,17 @@ import scrapy
 
 
 class FormationscraperItem(scrapy.Item):
-    # define the fields for your item here like:
     filiere = scrapy.Field()
     titre_formation = scrapy.Field()
-    # simplon_titre_certif = scrapy.Field()
-    # certif_france_competence = scrapy.Field()
     certif_rncp = scrapy.Field()
     certif_rs = scrapy.Field()
+    sessions = scrapy.Field()
+    id = scrapy.Field()
+
+
+class SessionscraperItem(scrapy.Item):
+    location = scrapy.Field()
+    date_debut = scrapy.Field()
+    id = scrapy.Field()
+
     
