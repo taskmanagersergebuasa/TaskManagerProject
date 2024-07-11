76732fbdc45eaccc96435be39c8baf48e568d801
EliasMez <elias.mezine@laposte.net>
2024-07-11 17:30:49 +0200

    integration des sessions et merge avec crawl certif

--- formationscraper/formationscraper/settings.py
+++ formationscraper/formationscraper/settings.py
@@ -66,7 +66,7 @@ RANDOMIZE_DOWNLOAD_DELAY = True
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
    # "formationscraper.pipelines.FormationscraperPipeline": 200,
-   "formationscraper.pipelines.CsvPipeline": 300,
+   # "formationscraper.pipelines.CsvPipeline": 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
